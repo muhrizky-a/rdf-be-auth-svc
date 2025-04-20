802d2f8ce653e0b28e0b0eb42b3320d1e1106ebc
muhrizky-a <muhrizky1337a@gmail.com>
2025-04-20 14:26:35 +0800

    add(cmd): main.go

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/ryakadev/rdf-be-auth-svc/delivery/grpc"
+	"github.com/ryakadev/rdf-be-auth-svc/domain"
+	"github.com/ryakadev/rdf-be-auth-svc/infrastructure"
+	"github.com/ryakadev/rdf-be-auth-svc/proto"
+	"github.com/ryakadev/rdf-be-auth-svc/repository"
+	"github.com/ryakadev/rdf-be-auth-svc/usecase"
+
+	gogrpc "google.golang.org/grpc"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3001"
+	}
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	db := infrastructure.ConnectDB()
+
+	if err := db.AutoMigrate(&domain.Scope{}); err != nil {
+		log.Fatalf("Failed to migrate: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	scopeRepo := repository.NewScopeRepository(db)
+	roleScopeRepo := repository.NewRoleScopeRepository(db)
+	scopeUC := usecase.NewScopeUsecase(scopeRepo, roleScopeRepo)
+	scopeGRPC := grpc.NewScopeGRPC(scopeUC)
+
+	gRPCServer := gogrpc.NewServer()
+	proto.RegisterScopeServiceServer(gRPCServer, scopeGRPC)
+
+	log.Println("gRPC server is running on port" + port)
+	err = gRPCServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("Failed to serve gRPC server over port "+port+": %v", err)
+	}
+}
