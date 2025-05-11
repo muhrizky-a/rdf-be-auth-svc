72abb30d892d1e94eba79dcf30f8799f01c7992c
muhrizky-a <muhrizky1337a@gmail.com>
2025-05-11 22:24:24 +0800

    refactor(migration): change migration execution from raw query to db/migrations directory

--- helper/migrations.go
+++ helper/migrations.go
@@ -2,21 +2,44 @@ package helper
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
-	"github.com/ryakadev/rdf-be-auth-svc/domain"
 	"gorm.io/gorm"
 )
 
-func RunMigrations(db *gorm.DB) error {
-	if err := db.AutoMigrate(&domain.Scope{}); err != nil {
-		log.Fatalf("Migration Scope failed: %v", err)
-		return err
-	}
+func runMigrationFromDatabaseFiles(db *gorm.DB, databasePaths []string) error {
+	for _, databasePath := range databasePaths {
+		// Read the SQL file content
+		path, err := filepath.Abs(databasePath)
+		if err != nil {
+			log.Fatalf("Error reading SQL file path: %v", err)
+			return err
+		}
 
-	if err := db.AutoMigrate(&domain.RoleScope{}); err != nil {
-		log.Fatalf("Migration RoleScope failed: %v", err)
-		return err
+		sqlFile, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatalf("Error reading SQL file: %v", err)
+			return err
+		}
+
+		// Execute the SQL command
+		sqlCommand := string(sqlFile)
+		if err := db.Exec(sqlCommand).Error; err != nil {
+			log.Printf("Error executing Scope migration command: %v", err)
+			return err
+		}
 	}
 
 	return nil
+}
+
+func RunMigrations(db *gorm.DB) error {
+	return runMigrationFromDatabaseFiles(
+		db,
+		[]string{
+			"./db/migrations/20250418024648_create-table-scopes.up.sql",
+			"./db/migrations/20250418101548_create-table-role-scopes.up.sql",
+		},
+	)
 }
