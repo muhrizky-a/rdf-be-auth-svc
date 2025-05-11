ccbcb0309a4e6d98129267ca53d2f7fef42bd035
muhrizky-a <muhrizky1337a@gmail.com>
2025-05-11 22:21:27 +0800

    refactor(migration): add timestamp on table role_scopes

--- db/migrations/20250418101548_create-table-role-scopes.up.sql
+++ db/migrations/20250418101548_create-table-role-scopes.up.sql
@@ -2,6 +2,9 @@ CREATE TABLE IF NOT EXISTS role_scopes (
     role_scope_id SERIAL PRIMARY KEY,
     role_id int NOT NULL,
     scope_id int NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE,
+    deleted_at TIMESTAMP WITH TIME ZONE,
     CONSTRAINT fk_scope
       FOREIGN KEY(scope_id)
         REFERENCES scopes(id)
